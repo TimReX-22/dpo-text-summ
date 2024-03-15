000f2e14b6d5ed1fc2b1a0ee3270d2f74cb210f8
Tim Reinhart <tim.reinhart@bluewin.ch>
2024-03-15 16:36:19 -0700

    updated main.sh

--- main.sh
+++ main.sh
@@ -1,16 +1,17 @@
 RUN_DPO=true
+RUN_SFT=false
 
 if $RUN_DPO; then
     python src/dpo.py \
-        --model_name_or_path='/dataNAS/people/onat/reward_model/Project/rlhf_checkpoints/flan-t5-xl_no_dpo' \
+        --model_name_or_path='./results/models_tuned/opi/flan-t5-xl/1' \
+        --output_dir="" \
         --per_device_train_batch_size 4 \
-        --max_steps 500 \
+        --max_steps 100 \
         --learning_rate 0.00001 \
         --gradient_accumulation_steps 2 \
         --logging_steps 10 \
         --eval_steps 1000 \
         --use_ref_model false \
-        --output_dir='/dataNAS/people/onat/reward_model/Project/output' \
         --optim rmsprop \
         --loss_type=sigmoid \
         --warmup_steps 100 \
@@ -25,4 +26,27 @@ if $RUN_DPO; then
         --lora_r 16 \
         --lora_alpha 16 \
         --measure_time
+
+    python src/run.py --model flan-t5-xl \
+        --model_path="./results/models_tuned/opi/flan-t5-xl/0" \
+        --dataset opi \
+        --n_samples 250 \
+        --dpo
+
+    python src/calc_metrics.py --model flan-t5-xl \
+        --dataset opi \
+        --n_samples 250 \
+        --dpo
+fi
+
+if $RUN_SFT; then
+    python src/sft.py --model flan-t5-xl --dataset opi
+
+    python src/run.py --model flan-t5-xl \
+        --dataset opi \
+        --n_samples 250
+
+    python src/calc_metrics.py --model flan-t5-xl \
+        --dataset opi \
+        --n_samples 250
 fi
